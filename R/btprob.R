31ef607121fa1d6b9af583fed464ab6d72d4e393
EllaKaye <ella.kaye@gmail.com>
2016-11-19 11:55:37 +0000

    argument checks on btprob

--- R/btprob.R
+++ R/btprob.R
@@ -1,8 +1,8 @@
 btprob <- function(pi) {
 
-  #if(!(!is.vector(pi) & !is.list(pi))) stop("1. pi should be a numeric vector or a list of numeric vectors")
-  #if(is.list(pi) & (sum(sapply(pi, is.numeric)) != length(pi))) stop("3. pi should be a numeric vector or a list of numeric vectors")
-  #if(is.vector(pi) & !is.numeric(pi)) stop("2. pi should be a numeric vector or a list of numeric vectors")
+  if(!is.vector(pi)) stop("pi should be a numeric vector or a list of numeric vectors")
+  if(!is.list(pi) & !is.numeric(pi)) stop("pi should be a numeric vector or a list of numeric vectors")
+  if(is.list(pi) & (sum(sapply(pi, is.numeric)) != length(pi))) stop("pi should be a numeric vector or a list of numeric vectors")
 
   btprob_vec <- function(pi) {
     mat <- pi * 1/outer(pi, pi, FUN = "+")
