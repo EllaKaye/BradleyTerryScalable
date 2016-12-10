8a77bba7732532af0c240bded862165469a89b6f
EllaKaye <ella.kaye@gmail.com>
2016-12-10 17:46:33 +0000

    experiment with as_df

--- R/as_df.R
+++ R/as_df.R
@@ -3,13 +3,43 @@ as_df <- function(sM) {
   if(class(sM) != "dgTMatrix") sM <- as(sM, "dgTMatrix")
 
   if (!is.null(rownames(sM)) & !is.null(colnames(sM))) {
-    df <- data.frame(player1 = rownames(sM)[sM@i + 1], player2 = rownames(sM)[sM@j + 1], expected_wins = sM@x)
+    df <- data.frame(winner = rownames(sM)[sM@i + 1], loser = rownames(sM)[sM@j + 1], fit = sM@x)
   }
 
   else {
-    df <- data.frame(player1 = sM@i + 1, player2 = sM@j + 1, num_wins = sM@x)
+    df <- data.frame(winner = sM@i + 1, loser = sM@j + 1, fit = sM@x)
 
   }
 
   return(df)
 }
+
+as_df1 <- function(fitted, N = 0, diag = 0) {
+  fitted[lower.tri(fitted, diag = TRUE)] <- 0
+
+  #N[lower.tri(N, diag = TRUE)] <- 0
+
+  as_df(fitted) # gives upper triangle
+
+  fitted
+}
+
+
+as_df2 <- function(sM, N) {
+
+  if(class(sM) != "dgTMatrix") sM <- as(sM, "dgTMatrix")
+
+  if (!is.null(rownames(sM)) & !is.null(colnames(sM))) {
+    df <- data.frame(player1 = rownames(sM)[sM@i + 1], player2 = rownames(sM)[sM@j + 1],
+                     fit1 = sM@x, fit2 = N@x - sM@x)
+  }
+
+  else {
+    df <- data.frame(winner = sM@i + 1, loser = sM@j + 1, fit = sM@x)
+
+  }
+
+  return(df)
+}
+
+
