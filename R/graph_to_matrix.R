fb3c4ad2738dccdad024057707881de610871726
EllaKaye <ella.kaye@gmail.com>
2016-12-10 18:34:07 +0000

    graph_to_matrix accounts for both weighted and unweighted igraph objects

--- R/graph_to_matrix.R
+++ R/graph_to_matrix.R
@@ -3,9 +3,10 @@ graph_to_matrix <- function(g) {
 
   # check that graph is an igraph object
 
-  if(!igraph::is.igraph(g))  stop("graph must be an igraph or network object")
+  if(!igraph::is.igraph(g))  stop("g must be an igraph object")
 
-  if (igraph::is.igraph(g)) W <- igraph::as_adjacency_matrix(g, sparse = TRUE, attr = "weight")
+  if (igraph::is.weighted(g)) W <- igraph::as_adjacency_matrix(g, sparse = TRUE, attr = "weight")
+  else W <- igraph::as_adjacency_matrix(g, sparse = TRUE)
 
   return(W)
 
