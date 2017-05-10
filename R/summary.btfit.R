cd4db70883acafb2a401160a542a103e17a45b89
David Firth <d.firth@warwick.ac.uk>
2017-05-10 19:59:03 +0100

    Initial commit of vcov and summary methods.

---
+++ R/summary.btfit.R
@@ -0,0 +1,17 @@
+summary.btfit <- function(object, ref = NULL, ...){
+    pvec <- object$pi
+    k <- length(pvec)
+    lambda <- log(pvec)
+    if (!is.null(ref)) {  ## The specified constraint is that one of the log-ability parameters is zero
+        if (ref %in% names(pvec)) ref <- which(names(pvec) == ref)
+        if (ref %in% 1:k) {
+            lambda <- lambda - lambda[ref]
+        } else stop("Invalid value for the 'ref' argument")
+    }
+    vc <- vcov(object, ref = ref)
+    se <- sqrt(diag(vc))
+    result <- data.frame(estimate = lambda, SE = se)
+    row.names(result) <- names(pvec)
+    class(result) <- c("summary.btfit", "data.frame")
+    return(result)
+}
