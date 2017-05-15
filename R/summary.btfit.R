f64287d13e6ff1842889e5c9842f1dc708f0333d
EllaKaye <ella.kaye@gmail.com>
2017-05-15 08:36:18 +0100

    use lambda instead of pi in summary.btfit

--- R/summary.btfit.R
+++ R/summary.btfit.R
@@ -2,20 +2,21 @@
 summary.btfit <- function(object, ref = NULL, ...){
   
   if (!inherits(object, "btfit")) stop("object should be a 'btfit' object")
-  
-    pvec <- object$pi
-    k <- length(pvec)
-    lambda <- log(pvec)
+
+    lambda <- object$lambda
+    K <- length(lambda)
+    # pi <- exp(lambda)
+
     if (!is.null(ref)) {  ## The specified constraint is that one of the log-ability parameters is zero
-        if (ref %in% names(pvec)) ref <- which(names(pvec) == ref)
-        if (ref %in% 1:k) {
+        if (ref %in% names(lambda)) ref <- which(names(lambda) == ref)
+        if (ref %in% 1:K) {
             lambda <- lambda - lambda[ref]
         } else stop("Invalid value for the 'ref' argument")
     }
     vc <- vcov(object, ref = ref)
     se <- sqrt(diag(vc))
     result <- data.frame(estimate = lambda, SE = se)
-    row.names(result) <- names(pvec)
+    row.names(result) <- names(lambda)
     class(result) <- c("summary.btfit", "data.frame")
-    return(result)
+    result
 }
