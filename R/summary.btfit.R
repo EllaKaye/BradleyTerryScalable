2c60c7d50630a3f70341de1644d40ebeb866b031
EllaKaye <ella.kaye@gmail.com>
2017-05-18 09:58:30 +0100

    combine_df argument to summary.btfit

--- R/summary.btfit.R
+++ R/summary.btfit.R
@@ -10,16 +10,24 @@ summary_vec <- function(pi, N, ref = NULL){
 }
 
 #' @export
-summary.btfit <- function(object, ref = NULL, ...){
+summary.btfit <- function(object, ref = NULL, combine_df = FALSE, ...){
     
     if (!inherits(object, "btfit")) stop("object should be a 'btfit' object")
     
     pi <- object$pi
     N <- object$N
     
-    if (is.list(pi)) result <- purrr::map2(pi, N, summary_vec, ref = ref)
+    if (is.list(pi)) {
+      result <- purrr::map2(pi, N, summary_vec, ref = ref)
+      
+      if (combine_df) {
+        comp_num <- 1:length(pi)
+        result <- purrr::map2(result, comp_num, ~ .x %>% mutate(component = .y)) %>%
+          dplyr::bind_rows()
+      }
+    }
     
-    else result <- summary_vec(pi, N, ref = ref)
+    else result <- summary_vec(pi, N, ref = ref) 
     
     result
 }
