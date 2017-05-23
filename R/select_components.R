87b3f059a21d91d3193d6412b8d1bf372478cfda
EllaKaye <ella.kaye@gmail.com>
2017-05-23 22:37:13 +0100

    select_components can return graph, and comps must be character vec

--- R/select_components.R
+++ R/select_components.R
@@ -1,9 +1,12 @@
 #' @export
 
-select_components <- function(btdata, comps) {
+select_components <- function(btdata, comps, return_graph = FALSE) {
 
   ### Check for correct data object, and extract elements
   if (!inherits(btdata, "btdata")) stop("btdata argument must be a 'btdata' object, as created by btdata() function.")
+  
+  if (!is.character(comps)) stop("comps should be a character vector")
+  if (return_graph & is.null(btdata$graph)) warning("There needs to be a graph component in btdata to return a graph here")
     
   components <- btdata$components
   wins <- btdata$wins
@@ -19,12 +22,13 @@ select_components <- function(btdata, comps) {
   sub_wins <- wins[unlist(sub_comps), unlist(sub_comps)] 
     
   # subset graph
-  #if (!is.null(btdata$graph)) {
-    
-  #}
+  if (!is.null(btdata$graph) & return_graph) {
+    graph <- btdata$graph
+    g <- igraph::induced_subgraph(graph, unlist(sub_comps))
+  }
   
   result <- list(wins = sub_wins, components = sub_comps)
-  # if (!is.null(btdata$graph)) result$graph <- g
+  if (!is.null(btdata$graph) & return_graph) result$graph <- g
   class(result) <- c("btdata", "list")
   result  
 }
