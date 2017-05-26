39916d50eb2542716d86d50fb93cb01fbc0dfe0c
EllaKaye <ella.kaye@gmail.com>
2017-05-26 07:15:22 +0100

    Allow subset by condition in select_components

--- R/select_components.R
+++ R/select_components.R
@@ -1,22 +1,41 @@
 #' @export
-
-select_components <- function(btdata, comps, return_graph = FALSE) {
+select_components <- function(btdata, subset, return_graph = FALSE) {
 
   ### Check for correct data object, and extract elements
   if (!inherits(btdata, "btdata")) stop("btdata argument must be a 'btdata' object, as created by btdata() function.")
   
-  if (!is.character(comps)) stop("comps should be a character vector")
   if (return_graph & is.null(btdata$graph)) warning("There needs to be a graph component in btdata to return a graph here")
     
   components <- btdata$components
   wins <- btdata$wins
   
-  # if there is only one component, there is no selecting to do
-  if (length(components) == 1) return(btdata)
+  # check that subset is of the correct form
+  if (!is.function(subset) & !is.character(subset) & !(is.logical(subset))) stop(
+    "subset is not of the correct form - see the documentation for more details."
+  )
   
+  if(is.logical(subset)) {
+    if (length(subset) != length(components)) stop("length(subset) == length(btdata$components) is not TRUE")
+  }
+  
+  # quick check on subset if function (on component 1)
+  if(is.function(subset)) {
+    test_of_function <- subset(components[[1]])
+    if (!is.logical(test_of_function)) stop("if subset is a function, it must return either TRUE or FALSE")
+    if (length(test_of_function) > 1) stop("if subset is a function, it must return either TRUE or FALSE")
+  }
+  # rely on purrr::keep's error message if length == 1 on comp 1, but > 1 elsewhere
+  
+  # correct way to test subset on all components (but may be too costly)
+  # if (purrr::map(components, subset) %>% map_int(length) %>% any(. > 1)) stop("if subset is a function, it must return either TRUE or FALSE")
+  
+  if(is.character(subset)) {
+    if(!all(subset %in% names(components))) stop("not all elements of subset are names of components")
+  }
 
   # select components
-  sub_comps <- components[comps]
+  if (is.character(subset)) sub_comps <- components[subset]
+  else sub_comps <- purrr::keep(components, subset)
   
   # subset wins
   sub_wins <- wins[unlist(sub_comps), unlist(sub_comps)] 
