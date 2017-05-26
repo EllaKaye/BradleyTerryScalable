d71e4961fab6e399a1b6e4a10a502a9290af7670
EllaKaye <ella.kaye@gmail.com>
2017-05-26 13:13:35 +0100

    summary.btfit now a list of two tibbles

--- R/summary.btfit.R
+++ R/summary.btfit.R
@@ -3,36 +3,53 @@ summary_vec <- function(pi, N, ref = NULL){
     lambda <- coef_vec(pi, ref)
     vc <- vcov_vec(pi, N, ref)
     se <- sqrt(diag(vc))
-    result <- dplyr::data_frame(item = names(lambda), estimate = lambda, SE = se)
-    # rownames(result) <- names(lambda)
-    class(result) <- c("summary.btfit", "tbl_df", "tbl", "data.frame")
+    if (!is.null(names(lambda))) item <- names(lambda)
+    else item <- 1:length(lambda)
+    result <- dplyr::data_frame(item = item, estimate = lambda, SE = se)
+    class(result) <- c("tbl_df", "tbl", "data.frame")
     result
 }
 
 #' @export
-summary.btfit <- function(object, ref = NULL, combine = FALSE, ...){
+component_summary_vec <- function(pi, iters, converged) {
+  
+  num_items <- length(pi)
+  
+  result <- dplyr::data_frame(num_items = num_items, iters = iters, converged = converged)
+  
+  class(result) <- c("tbl_df", "tbl", "data.frame")
+  result  
+}
+
+#' @export
+summary.btfit <- function(object, ref = NULL, ...){
     
     if (!inherits(object, "btfit")) stop("object should be a 'btfit' object")
     
     pi <- object$pi
     N <- object$N
-    
-    if (is.list(pi)) {
+    iters <- object$iters
+    converged <- object$converged
+
         
-      ## Restrict 'ref' value to NULL or 1 if there is >1 component
-      if (!(is.null(ref)) && (ref != 1)) stop("The value of 'ref' should be 1 or NULL")
+    ## Restrict 'ref' value to NULL or 1 if there is >1 component
+    if (!(is.null(ref)) && (ref != 1)) stop("The value of 'ref' should be 1 or NULL")
         
-      result <- purrr::map2(pi, N, summary_vec, ref = ref)
+    summary_by_comp <- purrr::map2(pi, N, summary_vec, ref = ref)
       
-      if (combine) {
-        comp_names <- names(pi)
-        result <- purrr::map2(result, comp_names, ~ .x %>% dplyr::mutate(component = .y)) %>%
-          dplyr::bind_rows()
-      }
-    }
+    comp_names <- names(pi)
+    summary_result <- purrr::map2(summary_by_comp, comp_names, ~ .x %>% dplyr::mutate(component = .y)) %>%
+        dplyr::bind_rows()
     
-    else result <- summary_vec(pi, N, ref = ref) 
     
-    #class(result) <- c("summary.btfit", "list")
+    component_summary_result <- purrr::pmap(list(pi, iters, converged), component_summary_vec) %>%
+      dplyr::bind_rows() %>% 
+      dplyr::mutate(component = comp_names) %>%
+      dplyr::select(component, num_items:converged)
+
+    
+    result <- list(summary_result = summary_result, component_summary_result = component_summary_result)
+    
+    class(result) <- c("summary.btfit", "list")
     result
 }
