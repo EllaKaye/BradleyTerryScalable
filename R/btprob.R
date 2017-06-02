224d3bc95c57c894e06ddb48c6ab9f73b6e8f3d8
EllaKaye <ella.kaye@gmail.com>
2017-06-02 15:55:01 +0100

    fix btprob

--- R/btprob.R
+++ R/btprob.R
@@ -8,7 +8,7 @@ as_df_btprob <- function(m) {
     stop("The package reshape2 is needed for as_df = TRUE in btprob. Please install it.",
          call. = FALSE)
   }
-
+  
   m[lower.tri(m, diag = TRUE)] <- NA
   
   # hack to avoid NOTES on global variables
@@ -20,7 +20,7 @@ as_df_btprob <- function(m) {
   out <- dplyr::mutate_if(out, is.factor, as.character)
   out <- dplyr::rename(out, "prob1wins" = value)
   out <- dplyr::mutate(out, "prob2wins" = 1 - prob1wins)
-
+  
   out
 }
 
@@ -35,7 +35,7 @@ as_df_btprob <- function(m) {
 #' If \eqn{G_W} is not fully connected, then a penalised strength parameter can be obtained using the method of Caron and Doucet (2012) (see \code{\link{btfit}}, with \code{a > 1}), which allows for a Bradley-Terry probability of any of the K items beating any of the others. Alternatively, the MLE can be found for each fully connected component of \eqn{G_W} (see \code{\link{btfit}}, with \code{a = 1}), and the probability of each item in each component beating any other item in that component can be found.
 #'
 #' @param object An object of class "btfit", typically the result \code{ob} of \code{ob <- btfit(..)}. See \code{\link{btfit}}.
-#' @param as_df Logical scalar, determining class of output. If \code{TRUE} (the default), the function returns a data frame. If \code{FALSE}, the function returns a matrix (or list of matrices).
+#' @param as_df Logical scalar, determining class of output. If \code{TRUE}, the function returns a data frame. If \code{FALSE} (the default), the function returns a matrix (or list of matrices).
 #'@param subset A character vector of names of components (i.e. a subset of names(object$pi))
 #' @return If \code{as_df = FALSE}, returns a matrix where the \eqn{i,j}-th element is the Bradley-Terry probability \eqn{p_{ij}}, or, if \eqn{G_W} is not fully-connected and \code{\link{btfit}} has been run with \code{a = 1}, a list of such matrices for each fully-connected component of \eqn{G_W}. If \code{as_df = TRUE}, returns a five-column data frame, where the first column is \code{item1}, the second column is \code{item2}, the third column is the Bradley-Terry probability that item 1 beats item 2 and the fourth column is the Bradley-Terry probability that item 2 beats item 1, and the fifth column is the component that the two items are in. If the original \code{btdata$wins} matrix has named dimnames, these will be the \code{colnames} for columns one and two. See Details.
 #' @references Bradley, R. A. and Terry, M. E. (1952). Rank analysis of incomplete block designs: 1. The method of paired comparisons. \emph{Biometrika}, \strong{39}(3/4), 324-345.
@@ -57,7 +57,7 @@ as_df_btprob <- function(m) {
 #' btprob(fit2, as_df = TRUE)
 #' btprob(fit3)
 #' @export
-btprob <- function(object, as_df = TRUE, subset = NULL) {
+btprob <- function(object, as_df = FALSE, subset = NULL) {
   
   if (!inherits(object, "btfit")) stop("Object should be a 'btfit' object")
   
@@ -68,20 +68,10 @@ btprob <- function(object, as_df = TRUE, subset = NULL) {
   if (!is.null(subset)) {
     if (!is.character(subset)) stop("subset should be a character vector")
     if(!all(subset %in% names(pi))) stop("not all elements of subset are names of components")
-    
-    #subset_in_btfit_methods_check(subset, pi)
-    
-    #if (is.character(subset)) {
-      pi <- pi[subset]
-      components <- components[subset]
-    #}
-    
-    #else {
-    #  purrr::keep(pi, subset)
-    #  purrr::keep(components, subset)
-    #}
+    pi <- pi[subset]
+    components <- components[subset]
   }
-
+  
   # set up names of dimnames  
   names_dimnames <- object$names_dimnames  
   names_dimnames_list <- list(names_dimnames)
@@ -90,7 +80,7 @@ btprob <- function(object, as_df = TRUE, subset = NULL) {
   p <- purrr::map(pi, btprob_vec)
   p <- purrr::map2(p, components, name_matrix_function)
   p <- purrr::map2(p, names_dimnames_list, name_dimnames_function)
-    
+  
   if (as_df) {
     comp_names <- names(pi)
     
@@ -110,9 +100,9 @@ btprob <- function(object, as_df = TRUE, subset = NULL) {
   }
   
   if (length(pi) == 1 & !as_df) {
-   if (names(pi) == "full_dataset") {
-     p <- p[[1]]
-   }
+    if (names(pi) == "full_dataset") {
+      p <- p[[1]]
+    }
   }
   
   p
