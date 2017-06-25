c4bdf50af63029e7625da117096c4a1d4ad0382a
EllaKaye <ella.kaye@gmail.com>
2017-06-25 09:15:27 +0100

    codes_to_counts doc update, remove vignette cache, add data to pkgdown

--- R/code_to_counts.R
+++ R/code_to_counts.R
@@ -5,7 +5,7 @@
 #' This function is needed in the \code{BradleyTerryScalable} workflow when the user data is stored in a three-column data frame where each row is a comparison between two items, and where the third column is NOT a count of the number of times the item in the first column beat the item in the second column. Rather, it could be that the third column is a code for which of the two items won (including the possibility of a tie), for example \code{"W1", "W2", "D"}. Or else, it could be that the third column gives the score only in relation to the first item, e.g. 1 for a win, 0 for a loss or 0.5 for a draw without there anywhere in the table being the corresponding record for the second item (i.e. respectively 0 for a loss, 1 for a win and 0.5 for a draw.)
 #' 
 #' 
-#' @param df A three-column data frame. Each row represents a comparison between two items. The first and second columns are the names of the first and second players respectively. The third column gives a code for who won. See Details and Examples.
+#' @param df A three-column data frame. Each row represents a comparison between two items. The first and second columns are the names of the first and second items respectively. The third column gives a code for which won. See Details and Examples.
 #' @param codes A numeric vector or character vector, of length two or three (depending on whether there are ties.) The first and second element gives the codes used if the first or second item won respectively. If there are ties, the third element gives the code used in that case. See Details and Examples.
 #' @return A four-column data frame where the first two columns are the name of the first and second item. The third and fourth column gives the wins count for the first and second item respectively: 1 for a win, 0 for a loss, and 0.5 each for a draw. This data frame is in the correct format to be passed to \code{\link{btdata}}
 #' @examples 
@@ -17,6 +17,9 @@
 #' codes_to_counts(df2, c(1, 0, .5))
 #' df3 <- data.frame(player1 = first, player2 = second, which_won = c(1,2,2,1))
 #' codes_to_counts(df3, c(1,2))
+#' codes_to_counts(BradleyTerryScalable::toy_data, c("W1", "W2", "D"))
+#' @author Ella Kaye
+#' @seealso \code{\link{btdata}}
 #'
 #' @export
 codes_to_counts <- function(df, codes) {
