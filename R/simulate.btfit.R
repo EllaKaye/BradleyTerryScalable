5bcbde7a737564cc029f9b21d2d1dc52df763c30
David Firth <d.firth@warwick.ac.uk>
2017-06-28 14:37:55 +0100

    Edits to simulate_BT: -- removed redundant call to as.matrix -- tidied up the documentation example

--- R/simulate.btfit.R
+++ R/simulate.btfit.R
@@ -1,25 +1,34 @@
 #' Simulate data from a specified Bradley-Terry model
 #'
 #' @param pi a numeric vector, with all values finite and positive.
-#' @param N  a symmetric, numeric matrix with dimensions the same as \code{length(pi)}.  The elements should
+#' @param N  a symmetric, numeric matrix with dimensions the same as
+#' \code{length(pi)}.  The elements should
 #' be numeric representations of non-negative integers.
 #' @param nsim  a scalar integer, the number of datasets to be generated.
-#' @param seed  an object specifying if and how the random number generator should be initialized (‘seeded’).
+#' @param seed  an object specifying if and how the random number generator
+#' should be initialized (‘seeded’).
 #' For details see \code{\link[stats]{simulate}}.
-#' @param result_class a character vector specifying whether the generated datasets should be of class
-#' "sparseMatrix" or of class "btdata".  If not specified, the first match among those alternatives is used.
-#' @return a list of length \code{nsim} of simulated datasets. If \code{result_class = "sparseMatrix"}, the datasets are sparse matrices with the same dimensions as \code{N}. If \code{result_class = "btdata"} then the datasets are "btdata" objects. See \code{\link{btdata}}
+#' @param result_class a character vector specifying whether the generated
+#' datasets should be of class
+#' "sparseMatrix" or of class "btdata".  If not specified, the first match among
+#' those alternatives is used.
+#' @return a list of length \code{nsim} of simulated datasets.
+#' If \code{result_class = "sparseMatrix"}, the datasets are sparse matrices
+#' with the same dimensions as \code{N}. If \code{result_class = "btdata"} then
+#' the datasets are "btdata" objects. See \code{\link{btdata}}
 #' @seealso \code{\link{btfit}}, \code{\link{btdata}}
 #' @author David Firth
-#' @examples 
+#' @examples
 #' set.seed(1)
 #' n <- 6
-#' W <- matrix(rpois(n ^ 2, lambda = 5), n, n)
-#' N <- W + t(W)
-#' pi <- exp(rnorm(n)/4)  
-#' simulate_BT(pi, N, seed = 6)
+#' N <- matrix(rpois(n ^ 2, lambda = 1), n, n)
+#' N <- N + t(N) ; diag(N) <- 0
+#' p <- exp(rnorm(n)/4)
+#' names(p) <- rownames(N) <- colnames(N) <- letters[1:6]
+#' simulate_BT(p, N, seed = 6)
 #' @export
-simulate_BT <- function(pi, N, nsim = 1, seed = NULL, result_class = c("sparseMatrix", "btdata")){
+simulate_BT <- function(pi, N, nsim = 1, seed = NULL,
+                        result_class = c("sparseMatrix", "btdata")){
 
   ## A simulate function that takes a vector pi and a matrix N as its arguments
 
@@ -34,7 +43,8 @@ simulate_BT <- function(pi, N, nsim = 1, seed = NULL, result_class = c("sparseMa
     }
 
   #  Check the arguments
-  if ((!is.matrix(N)) && (!inherits(N, "sparseMatrix")) && !inherits(N, "denseMatrix")) stop("N is not a matrix")
+  if ((!is.matrix(N)) && (!inherits(N, "sparseMatrix")) &&
+      !inherits(N, "denseMatrix")) stop("N is not a matrix")
   dN <- dim(N)
   if (diff(dN) != 0) stop("N is not a square matrix")
   if (any(N < 0)) stop("N has one or more negative elements")
@@ -43,11 +53,14 @@ simulate_BT <- function(pi, N, nsim = 1, seed = NULL, result_class = c("sparseMa
   if (!is.numeric(pi)) stop("pi is not numeric")
   if (any (pi < 0)) stop("pi has one or more negative elements")
   result_class <- match.arg(result_class)
-  if (!(result_class %in% c("sparseMatrix", "btdata"))) stop("invalid value of result.class")
+  if (!(result_class %in% c("sparseMatrix", "btdata"))){
+      stop("invalid value of result.class")
+  }
 
-  template <- N <- methods::as(N, "dgCMatrix")  ## template is the matrix container for a single sample
+  template <- N <- methods::as(N, "dgCMatrix")
+  ## template is the matrix container for a single sample
 
-  N <- as.matrix(Matrix::summary(N))
+  N <- Matrix::summary(N)
   i <- N[, "i"]
   j <- N[, "j"]
   lower <- i > j               ## lower-triangle index vector
@@ -75,9 +88,7 @@ simulate_BT <- function(pi, N, nsim = 1, seed = NULL, result_class = c("sparseMa
   }
 
   result <- lapply(result, fill_template)
-
   attr(result, "seed") <- RNGstate
-
   return(result)
 }
 
@@ -85,7 +96,7 @@ simulate_BT <- function(pi, N, nsim = 1, seed = NULL, result_class = c("sparseMa
 #'
 #' @param object An object of class "btfit", typically the result of \code{ob} of \code{ob <- btfit(..)}. This object must only have one component, i.e. \code{length(object$pi) == 1}.
 #' @param ... Other arguments
-#' @examples 
+#' @examples
 #' citations_btdata <- btdata(BradleyTerryScalable::citations)
 #' fit1 <- btfit(citations_btdata, 1)
 #' simulate(fit1, nsim = 2, seed = 1)
